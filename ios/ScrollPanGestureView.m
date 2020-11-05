b9eafabdb6000e61f9f1c6c37d41018554057941
Jacob Parker <jacobparker1992@gmail.com>
2020-11-05 18:38:28 +0000

    .

--- ios/ScrollPanGestureView.m
+++ ios/ScrollPanGestureView.m
@@ -4,7 +4,17 @@
 
 @implementation ScrollPanGestureView {
   UIScrollView *_scrollview;
+
+  /* disableGestureWhenMomentumScrolling state */
   BOOL _wasMomentumScrolling;
+
+  /* cancelGestureAfterDraggingScrollBarToTop state */
+  int _numRegularScrollEvents;
+  int _numScrollBarScrollEvents;
+  CGFloat _lastContentOffsetY;
+  CGFloat _lastGestureTranslateY;
+
+  /* General event tracking state */
   BOOL _lastGestureEventWasPanGesture;
   CGFloat _upwardsGestureOriginY;
   CGFloat _translateY;
@@ -15,10 +25,17 @@
   self = [super init];
   if (self) {
     _scrollview = nil;
+
     _wasMomentumScrolling = NO;
+
+    _numRegularScrollEvents = 0;
+    _numScrollBarScrollEvents = 0;
+    _lastContentOffsetY = 0;
+
     _lastGestureEventWasPanGesture = NO;
     _upwardsGestureOriginY = 0;
     _translateY = 0;
+
     _enabled = YES;
     _maxUpwardsDisplacement = 0;
     _maxDownwardsDisplacement = 0;
@@ -77,38 +94,83 @@
     return;
   }
 
-  if (recognizer.state == UIGestureRecognizerStateBegan) {
-    _wasMomentumScrolling =
-      _disableGestureWhenMomentumScrolling &&
-      _scrollview.isDecelerating;
-  } else if (_wasMomentumScrolling) {
-    return;
-  }
+  CGFloat hackTopOffset = -2;
 
   CGFloat offsetY = _scrollview.contentOffset.y;
   BOOL downwardsGestureEnabled = _maxDownwardsDisplacement > 0;
   BOOL upwardsGestureEnabled = _maxUpwardsDisplacement > 0;
 
-  if (
-    _cancelGestureAfterDraggingScrollBarToTop &&
-    recognizer.state == UIGestureRecognizerStateChanged &&
-    downwardsGestureEnabled &&
-    offsetY < 0 &&
-    [recognizer velocityInView:_scrollview].y < 0
-  ) {
-    recognizer.enabled = NO;
-    recognizer.enabled = YES;
-    _scrollview.contentOffset = CGPointMake(_scrollview.contentOffset.x, 0);
-    _onGestureEnd([self eventData:recognizer cancelled:YES]);
-    return;
+  if (_disableGestureWhenMomentumScrolling) {
+    if (recognizer.state == UIGestureRecognizerStateBegan) {
+      BOOL scrollViewIsOverScrolling =
+        offsetY < hackTopOffset ||
+        offsetY > (_scrollview.contentSize.height - _scrollview.frame.size.height);
+
+      _wasMomentumScrolling =
+        (_scrollview.isDecelerating || scrollViewIsOverScrolling);
+    } else if (_wasMomentumScrolling) {
+      return;
+    }
   }
 
+  if (_cancelGestureAfterDraggingScrollBarToTop) {
+    if (recognizer.state == UIGestureRecognizerStateBegan) {
+      _numRegularScrollEvents = 0;
+      _numScrollBarScrollEvents = 0;
+      _lastContentOffsetY = offsetY;
+    } else if (recognizer.state == UIGestureRecognizerStateChanged) {
+      CGFloat gestureTranslateY = [recognizer translationInView:nil].y;
+      CGFloat gestureDeltaY = gestureTranslateY - _lastGestureTranslateY;
+      CGFloat scrollDeltaY = offsetY - _lastContentOffsetY;
+
+      BOOL dragUp = gestureDeltaY < 0;
+      BOOL dragDown = gestureDeltaY > 0;
+      BOOL scrollUp = scrollDeltaY < 0;
+      BOOL scrollDown = scrollDeltaY > 0;
+
+      /*
+       This is a heuristic for determining whether the scrollview is being dragged by the
+       scroll bar or by regular scrolling
+
+       Taking a single reading has a few false positives, but no false negatives
+
+       Apply a further heuristic to minimise false positives (below)
+       */
+      if ((dragDown && scrollUp) || (dragUp && scrollDown)) {
+        _numRegularScrollEvents += 1;
+      } else if ((dragUp && scrollUp) || (dragDown && scrollDown)) {
+        _numScrollBarScrollEvents += 1;
+      }
+
+      _lastContentOffsetY = offsetY;
+      _lastGestureTranslateY = gestureTranslateY;
+    }
+
+    BOOL isDraggingFromScrollBar =
+      _numRegularScrollEvents == 0 &&
+      _numScrollBarScrollEvents > 2;
+
+    if (
+      recognizer.state == UIGestureRecognizerStateChanged &&
+      downwardsGestureEnabled &&
+      isDraggingFromScrollBar &&
+      offsetY < 0
+    ) {
+      recognizer.enabled = NO;
+      recognizer.enabled = YES;
+      _scrollview.contentOffset = CGPointMake(_scrollview.contentOffset.x, 0);
+      _onGestureEnd([self eventData:recognizer cancelled:YES]);
+      return;
+    }
+  }
+
+  /* General event tracking */
   switch (recognizer.state) {
     case UIGestureRecognizerStatePossible:
       break;
     case UIGestureRecognizerStateBegan: {
       _lastGestureEventWasPanGesture = NO;
-      _upwardsGestureOriginY = MAX(_scrollview.contentOffset.y, 0);
+      _upwardsGestureOriginY = MAX(offsetY, 0);
       _translateY = 0;
 
       if (_onGestureStart != nil) {
